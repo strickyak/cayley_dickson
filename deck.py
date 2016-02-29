f40c25e6ca7d195a164f8d67794577ceefebdef6
strick <strick@yak.net>
2016-02-28 20:57:11 -0800

    deck.py to generate 1, 2, & 3 letter cards.

--- deck.py
+++ deck.py
@@ -1,4 +1,3 @@
-
 w = []
 
 for a in ["a", ""]:
@@ -7,14 +6,43 @@ for a in ["a", ""]:
       for x in ["x", ""]:
         for y in ["y", ""]:
           z = a+b+c+x+y
-          w.append(z if z else "-1")
+          z = z if z else "-1"
+          if len(z) in [1, 2, 3]:
+            w.append(z)
+
+for a in ["a", ""]:
+  for b in ["b", ""]:
+    for c in ["c", ""]:
+      for x in ["x", ""]:
+        for y in ["y", ""]:
+          z = a+b+c+x+y
+          z = z if z else "-1"
+          if len(z) in [2,]:
+            w.append(z)
+
+w.reverse()
 
 for x in range(8):
     print ""
+    print "%8s%12s%12s%12s" % (w.pop(), w.pop(), w.pop(), w.pop())
     print ""
     print ""
-    print "%10s%20s%20s%20s" % (w.pop(), w.pop(), w.pop(), w.pop())
-    print ""
-    print ""
-    print ""
-    print ""
+
+if False:
+  for a in ["a", ""]:
+    for b in ["b", ""]:
+      for c in ["c", ""]:
+        for x in ["x", ""]:
+          for y in ["y", ""]:
+            z = a+b+c+x+y
+            w.append(z if z else "-1")
+
+  for x in range(8):
+      print ""
+      print ""
+      print ""
+      print "%10s%20s%20s%20s" % (w.pop(), w.pop(), w.pop(), w.pop())
+      print ""
+      print ""
+      print ""
+      print ""
